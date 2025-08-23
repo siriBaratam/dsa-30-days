95b903b938da36d8779bf0277b70125b7dccce67
siriBaratam <siribaratam151@gmail.com>
2025-08-23 22:48:44 +0530

    added merge code

---
+++ Day4/mergeSortedArray.java
@@ -0,0 +1,32 @@
+
+class Solution {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] res = new int[m + n];
+        int k = 0, i = 0, j = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                res[k] = nums1[i];
+                i++;
+                k++;
+            } else {
+                res[k] = nums2[j];
+                j++;
+                k++;
+            }
+        }
+        while (i < m) {
+            res[k] = nums1[i];
+            i++;
+            k++;
+        }
+        while (j < n) {
+            res[k] = nums2[j];
+            j++;
+            k++;
+        }
+        for (i = 0; i < res.length; i++) {
+            nums1[i] = res[i];
+        }
+    }
+}
